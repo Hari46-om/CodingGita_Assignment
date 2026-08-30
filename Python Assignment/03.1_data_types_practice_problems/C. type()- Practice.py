c49dc88b8371c4e6ad78cd5a4115a74053d84f3a
Hariom yadav <hariom.yadav.cg@gmail.com>
2026-08-30 23:03:54 +0530

    Add files via upload

---
+++ Python Assignment/03.1_data_types_practice_problems/C. type()- Practice.py
@@ -0,0 +1,40 @@
+# Answer
+
+# Qusetion number=18
+# x = 25
+# print(type(x))
+# <class 'int'>
+
+# Qusetion number=19
+# price = 99.50
+# print(type(price))
+# <class 'float'>
+
+# Qusetion number=20
+# name = "Python"
+# print(type(name))
+# <class 'str'>
+
+# Qusetion number=21
+# is_active = True
+# print(type(is_active))
+# <class 'bool'>
+
+# Qusetion number=22
+# data = None
+# print(type(data))
+# <class 'NoneType'>
+
+# Qusetion number=23
+# integer_num = 25          # int
+# float_num = 12.5          # float
+# text = "Hello Python"     # str
+# is_student = True         # bool
+# complex_num = 3 + 4       # complex
+
+# # Display values and their types
+# print(integer_num, type(integer_num))
+# print(float_num, type(float_num))
+# print(text, type(text))
+# print(is_student, type(is_student))
+# print(complex_num, type(complex_num))
---
+++ Python Assignment/03.1_data_types_practice_problems/D. Practical Problems.py
@@ -0,0 +1,73 @@
+# Answer
+
+# Question number=24
+
+# student_name = "Hariom"
+# student_age = 18
+# student_height = 5.7
+# is_student = True
+# student_result = None
+
+# print("Student Name:", type(student_name))
+# print("Student Age:", type(student_age))
+# print("Student Height:", type(student_height))
+# print("Is Student:", type(is_student))
+# print("Student Result:", type(student_result))
+
+# Question number=25
+
+# a=50
+# b=50.0
+# c="50"
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# Qusetion number=26
+
+# a=True
+# b="True"
+# print(type(a))
+# print(type(b))
+
+# Question number=27
+
+# a=None
+# b="None"
+# print(type(a))
+# print(type(b))
+
+# Question number=28
+
+# value=56
+# print(type(value))
+# value="56"
+# print(type(value))
+
+# In first the type is integer
+# In second time the number is string
+
+# Question number=29
+
+# product_name = "Laptop"
+# product_quantity = 10 
+# product_price = 49999.99
+# is_available = True
+# product_discount = None
+
+# print("Product Name:", product_name, "-", type(product_name))
+# print("Product Quantity:", product_quantity, "-", type(product_quantity))
+# print("Product Price:", product_price, "-", type(product_price))
+# print("Available:", is_available, "-", type(is_available))
+# print("Product Discount:", product_discount, "-", type(product_discount))
+
+# Question number=30
+
+# print(10, "-", type(10))
+# print(10.0, "-", type(10.0))
+# print("10", "-", type("10"))
+# print(True, "-", type(True))
+# print("True", "-", type("True"))
+# print(None, "-", type(None))
+# print("None", "-", type("None"))
